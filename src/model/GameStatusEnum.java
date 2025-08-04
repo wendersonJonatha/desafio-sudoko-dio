2f33a08545b7246ce918dda3cc47fc54869f9a31
wenderson jonatha <wendersonkw1@gmail.com>
2025-08-04 19:13:03 -0300

    Ciração das Class Board, GameStatusEnum e Scape.

---
+++ src/model/GameStatusEnum.java
@@ -0,0 +1,18 @@
+package model;
+
+public enum GameStatusEnum {
+
+    NON_STARTED("não iniciado"),
+    INCOMPLETE("incompleto"),
+    COMPLETE("completo");
+
+    private String label;
+
+    GameStatusEnum(final String label){
+        this.label = label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+}
